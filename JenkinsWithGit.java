9f3f8d1ce1f9d6bd9386c8bef13ee52192d2f4fe
AbdulrhmanAissa <AbdulrhmanAissa@hotmail.com>
2023-02-12 11:19:51 +0300

    Pushing JenkinsWithGit.java

---
+++ JenkinsWithGit.java
@@ -0,0 +1,13 @@
+package miscelleneous;
+
+public class JenkinsWithGit {
+	public static void main(String[] args) {
+		//code to print Unicode characters
+		int charPerLine=0;
+		for (int i = 33; 1 < 256; i++, charPerLine++)
+		{
+			System.out.print((char)i+((charPerLine%10=-0)?"In":"It"));
+			System.out.print ();
+		}
+	}
+}
